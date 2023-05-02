5be914b258f022b65d58e0c9c60370467ffe21e0
Tomas Delgado <tdelgadoandrade@frba.utn.edu.ar>
2023-05-02 12:41:14 -0300

    Resurreccion del ejercicio

--- pdepFuncionalEjercicioQuimica/src/Lib.hs
+++ pdepFuncionalEjercicioQuimica/src/Lib.hs
@@ -2,6 +2,26 @@ import Data.List
 
 --1) Modelar las siguientes sustancias
 --a. El hidrógeno y el oxígeno
+
+data Componente = Componente Sustancia Int deriving (Show)
+data Sustancia= 
+              Elemento{
+                nombre 				  :: String, 
+                simboloQuimico  :: String, 
+                numeroAtomico   :: Int,
+                tipoDeSustancia :: String
+              } 
+              |Compuesto{
+                componente :: [Componente],
+                tipo :: String,
+                nombre :: String
+              } 
+  deriving (Show)
+
+oxigeno :: Sustancia
+oxigeno = Elemento "Oxigeno" "O" 8 "No Metal"
+
+{-
 data Elemento = Elemento {
 nombre 				  :: String, 
 simboloQuimico  :: String, 
@@ -13,47 +33,30 @@ data Componente = Componente {
   elemento :: Elemento,
   cantidadElemento :: Int
 } deriving (Show)
+-}
 
-oxigeno :: Elemento
-oxigeno = Elemento "Oxigeno" "O" 8 "No Metal"
+hidrogeno :: Sustancia
+hidrogeno = Elemento "Hidrogeno" "H" 1 "gas noble"
 
-hidrogeno :: Elemento
-hidrogeno = Elemento "Hidrogeno" "H" 1 "Gas noble"
-
-cloro :: Elemento
+cloro :: Sustancia
 cloro = Elemento "Cloro" "Cl" 17 "halogeno"
 
-sodio :: Elemento
+sodio :: Sustancia
 sodio = Elemento "Sodio" "Na" 11 "metal"
 --b. El agua, sustancia compuesta por 2 hidrógenos y 1 un oxígeno.
 
-data Compuesto = Compuesto [Componente] String String          deriving (Show)
 
-agua :: Compuesto
-agua = Compuesto [Componente hidrogeno 2, Componente oxigeno 2] "No metal" "Agua"
-sal:: Compuesto
-sal = Compuesto [Componente cloro 1 , Componente sodio 1 ] "No metal" "sal"
---type Compuesto = [Componente]
+agua :: Sustancia
+agua = Compuesto [Componente hidrogeno 2, Componente oxigeno 2] "No metal" "agua"
+sal:: Sustancia
+sal = Compuesto [Componente cloro 1 , Componente sodio 1] "No metal" "sal"
+
 
 -- 2) 
 
-criterioCalor :: Compuesto -> Bool
-criterioCalor (Compuesto compuesto _ _) = (any (compararMetal) compuesto) || (any (compararHalogeno) compuesto)
-
-criterioElectricidad :: Compuesto -> Bool
-criterioElectricidad  (Compuesto compuesto _ _) = (any (compararGasNoble) compuesto) || (any (compararMetal) compuesto)
-
-conduceBien :: Compuesto -> Bool
-conduceBien compuesto = criterioElectricidad compuesto || criterioCalor compuesto
-
-compararMetal :: Componente -> Bool
-compararMetal sustancia =  tipoDeSustancia (elemento sustancia) == "Metal"
-
-compararGasNoble :: Componente -> Bool
-compararGasNoble sustancia =  tipoDeSustancia (elemento sustancia) == "Gas Noble"
-
-compararHalogeno :: Componente -> Bool
-compararHalogeno sustancia =  tipoDeSustancia (elemento sustancia) == "Halogeno"
+conduceBien :: Sustancia -> Bool
+conduceBien (Compuesto _ tipo _)  =  (tipo == "gas noble" || tipo == "metal") || (tipo == "halogeno" || tipo == "metal")
+conduceBien (Elemento _ _ _ tipo) = (tipo == "gas noble" || tipo == "metal" ) || (tipo == "halogeno" || tipo == "metal")
 
 -- 3)
 
@@ -66,13 +69,15 @@ nombreDeUnion nombre
 -- 4)  
 
 combinarNombres :: String -> String -> String 
-combinarNombres elemento1 elemento2 = nombreDeUnion elemento1 ++ " de " ++ elemento2
-
+combinarNombres elemento1 elemento2 = (nombreDeUnion elemento1) ++ " de " ++ elemento2
 -- 5)
-mezclar :: Componente -> Componente -> Compuesto
-mezclar (Componente elemento1 cantidad1) (Componente elemento2 cantidad2)   = Compuesto [(Componente elemento1 cantidad1),(Componente elemento2 cantidad2)] "No metal" (combinarNombres (nombre elemento1) (nombre elemento2))   
 
+
+mezclar :: Componente -> Componente -> Sustancia
+mezclar (Componente element1 cantidad1) (Componente element2 cantidad2)   = Compuesto [(Componente element1 cantidad1),(Componente element2 cantidad2)] "No metal" (combinarNombres (nombre element1) (nombre element2))
 -- 6)
 
-formula :: Compuesto -> String
-formula (Compuesto componentes tipo nombre) = 
+formula :: Sustancia -> String
+formula (Elemento _ simboloQuimico _ _) = simboloQuimico
+
+formula (Compuesto lista _ _) = 
